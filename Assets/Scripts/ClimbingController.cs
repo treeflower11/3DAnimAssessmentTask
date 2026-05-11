5b644749020bd5dc32747936b526933e61b547a4
treeflower11 <165136469+treeflower11@users.noreply.github.com>
2026-05-12 00:03:47 +1000

    code cleanup
    removed a debuglog lol

--- Assets/Scripts/ClimbingController.cs
+++ Assets/Scripts/ClimbingController.cs
@@ -44,7 +44,6 @@ public class ClimbingController : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("hello");
         if (other.CompareTag("NextScene"))
         {
             sceneDirector.GoToNextSubScene();
