2dd278bf1bcdc6791bfdfad6691fabdf24a40f65
treeflower11 <165136469+treeflower11@users.noreply.github.com>
2026-05-29 15:50:03 +1000

    interactive sequence base complete

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Unity.Cinemachine;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -5,27 +6,28 @@ using UnityEngine.Playables;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private GameObject givePaperInstructions;
+    [SerializeField] private GameObject moveInstructions;
     private InputAction moveAction;
+    private SceneDirector sceneDirector;
     private InputAction sprintAction;
-    private CinemachineCamera followPlayer;
+    private InputAction giveAction;
     private Rigidbody rb;
     private float moveSpeed = 5f;
     private float turnSpeed = 150f;
     private float sprintBoost = 1.5f;
     private Animator anim;
+    private Coroutine givePaper;
 
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         sprintAction = InputSystem.actions.FindAction("Sprint");
+        giveAction = InputSystem.actions.FindAction("Interact");
         rb = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
-        followPlayer = GetComponentInChildren<CinemachineCamera>();
-        if (followPlayer)
-        {
-            followPlayer.enabled = true;
-            followPlayer.Prioritize();
-        }
+        sceneDirector = GameObject.Find("SceneDirector").GetComponent<SceneDirector>();
+        ShowMoveInstructions();
     }
 
     void Update()
@@ -40,6 +42,33 @@ public class PlayerController : MonoBehaviour
             PlayableDirector sequence = other.GetComponentInChildren<PlayableDirector>();
             sequence.Play();
         }
+        else if (other.CompareTag("PaperRange"))
+        {
+            ShowGiveInstructions();
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("PaperRange"))
+        {
+            ShowMoveInstructions();
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        // Debug.Log("I'm in a trigger");
+        if (other.CompareTag("PaperRange") && giveAction.triggered)
+        {
+            givePaper ??= StartCoroutine(GivePaper());
+        }
+    }
+
+    private IEnumerator GivePaper()
+    {
+        if (sceneDirector) sceneDirector.GoToNextSubScene();
+        yield return null;
     }
 
     private void Move()
@@ -58,5 +87,17 @@ public class PlayerController : MonoBehaviour
         transform.Rotate(0, turnSpeed * rightInput * Time.deltaTime, 0);
         anim.SetBool("IsWalking", forwardInput != 0);
         anim.SetFloat("WalkSpeed", forwardInput);
+    }
+
+    private void ShowMoveInstructions()
+    {
+        if (moveInstructions) moveInstructions.SetActive(true);
+        if (givePaperInstructions) givePaperInstructions.SetActive(false);
+    }
+
+    private void ShowGiveInstructions()
+    {
+        if (moveInstructions) moveInstructions.SetActive(false);
+        if (givePaperInstructions) givePaperInstructions.SetActive(true);
     }
 }
